dc13dcd8fe803d51099e500fd49ea4361f049747
Justin Vasconcelos <jvasconcelos1990@gmail.com>
2026-01-02 22:13:02 -0500

    completed preprocessing section of course

--- rag-search-engine/cli/keyword_search_cli.py
+++ rag-search-engine/cli/keyword_search_cli.py
@@ -3,32 +3,34 @@
 import argparse
 import json
 import string
+from nltk.stem import PorterStemmer
 
 stop_words = []
 
-def remove_stop_words(tokens: list[str]):
+def clean_tokens(tokens: list[str]):
     if len(stop_words) < 1:
         with open('./data/stopwords.txt', 'r') as file:
             for line in file:
                 stop_words.append(line.strip())
-    
-    for token in tokens:
-        if token in stop_words:
-            tokens.remove(token)
+
+    stemmer = PorterStemmer()
+
+    # Clear and rebuild the list with stemmed, non-stop-word tokens
+    cleaned = [stemmer.stem(token) for token in tokens if token not in stop_words]
+    tokens.clear()
+    tokens.extend(cleaned)
 
 def search_movies(query: str):
     with open('./data/movies.json', 'r') as file:
         data = json.load(file)           
 
-    translator = str.maketrans('', '', string.punctuation)
-    
+    translator = str.maketrans('', '', string.punctuation)    
 
     #clean and get query tokens
     query_clean = query.lower().translate(translator)
     query_tokens = query_clean.split(' ')
 
-    #remove stop words in memory 
-    remove_stop_words(query_tokens)
+    clean_tokens(query_tokens)
 
     hits = 0
     for movie in data['movies']:
@@ -36,9 +38,12 @@ def search_movies(query: str):
             return
         
         title_clean = movie['title'].lower().translate(translator)        
+        title_tokens = title_clean.split(' ')
+
+        clean_tokens(title_tokens)
 
         for qt in query_tokens:
-            if qt in title_clean:
+            if qt in title_tokens:
                 print(movie['title'])
                 hits += 1 
                 break
