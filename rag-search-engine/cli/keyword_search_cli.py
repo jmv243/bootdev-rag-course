81e60450d1fb108d5fe09039de5ccfd1ad993339
Justin Vasconcelos <jvasconcelos1990@gmail.com>
2026-01-02 21:52:00 -0500

    added stop words

--- rag-search-engine/cli/keyword_search_cli.py
+++ rag-search-engine/cli/keyword_search_cli.py
@@ -4,27 +4,44 @@ import argparse
 import json
 import string
 
+stop_words = []
+
+def remove_stop_words(tokens: list[str]):
+    if len(stop_words) < 1:
+        with open('./data/stopwords.txt', 'r') as file:
+            for line in file:
+                stop_words.append(line.strip())
+    
+    for token in tokens:
+        if token in stop_words:
+            tokens.remove(token)
+
 def search_movies(query: str):
-    data = {}
     with open('./data/movies.json', 'r') as file:
-        data = json.load(file)       
+        data = json.load(file)           
 
     translator = str.maketrans('', '', string.punctuation)
-    hits = 0
+    
 
     #clean and get query tokens
     query_clean = query.lower().translate(translator)
     query_tokens = query_clean.split(' ')
 
+    #remove stop words in memory 
+    remove_stop_words(query_tokens)
+
+    hits = 0
     for movie in data['movies']:
+        if hits >= 5:
+            return
+        
         title_clean = movie['title'].lower().translate(translator)        
 
         for qt in query_tokens:
-            if hits >= 5:
-                return
             if qt in title_clean:
                 print(movie['title'])
-                hits += 1            
+                hits += 1 
+                break
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
